3acdc1c5b21b91d20399b89e16b582ed6606d946
Shivamyogi22 <shivamyogi22@gmail.com>
2024-05-30 13:42:24 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-// App.js
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"; // Import Navigate
 import CountTimer from "./components/CountTimer";
 import SearchUser from "./components/SearchUser";
 import Navbar from "./components/Navbar"; // Import Navbar component
@@ -11,6 +10,8 @@ function App() {
             <div className="App">
                 <Navbar />
                 <Routes>
+                    {/* Set default route to CountTimer */}
+                    <Route path="/" element={<Navigate to="/count-timer" />} />
                     <Route path="/count-timer" element={<CountTimer />} />
                     <Route path="/search-user" element={<SearchUser />} />
                 </Routes>
